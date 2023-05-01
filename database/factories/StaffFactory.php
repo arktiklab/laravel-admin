5ee4a69fca306997abb56a05a3932efa2b785638
Faisal Ahmed <foysal20x@gmail.com>
2023-05-01 22:04:04 +0600

    add factory

--- database/factories/StaffFactory.php
+++ database/factories/StaffFactory.php
@@ -1,19 +1,42 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Arktiklab\LaravelAdmin\Database\Factories;
 
+use Arktiklab\LaravelAdmin\Models\Staff;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
-/*
-class ModelFactory extends Factory
+class StaffFactory extends Factory
 {
-    protected $model = YourModel::class;
+    protected $model = Staff::class;
 
     public function definition()
     {
         return [
-
+            'first_name' => $this->faker->firstName(),
+            'last_name' => $this->faker->lastName(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
         ];
     }
+
+    public function superAdmin()
+    {
+        return $this->state(function (array $attributes) {
+            return ['super_admin' => true];
+        });
+    }
+
+    public function unverified()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'email_verified_at' => null,
+            ];
+        });
+    }
 }
-*/
